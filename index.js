ddb16e50dc5536d90a9b821ca54f23baa8186b35
GwapeDwink <GwapeDwink@users.noreply.github.com>
2017-12-19 18:32:51 +0000

    back to html

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ var http = require('http').Server(express);
 var io = require('socket.io')(http);
 
 
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', function(req, res,next) {
+      res.sendFile(__dirname + '/index.html');
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  
+
   io.on('connection', function(socket){
     console.log('a user connected');
   });
