fa9a8790f6d7554574d553d9a193febefedbc272
GwapeDwink <GwapeDwink@users.noreply.github.com>
2017-12-19 18:14:55 +0000

    fix resource

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const PORT = process.env.PORT || 5000
 var http = require('http').Server(express);
 var io = require('socket.io')(http);
 
-express.use('/resources', express.static(path.join(__dirname, 'resources')))
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
