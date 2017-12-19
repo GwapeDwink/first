a96731899630a798b79f6673bcbb32f22006574b
GwapeDwink <GwapeDwink@users.noreply.github.com>
2017-12-19 16:45:06 +0000

    Changed port output

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const path = require('path')
+var port = process.env.PORT || 5000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-// use port 3000 unless there exists a preconfigured port
-var port = process.env.port || 3000;
-
-app.listen(port);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
