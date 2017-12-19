6f92f5815dd87e9b997c3c12a5f2d2eba68e4bab
GwapeDwink <GwapeDwink@users.noreply.github.com>
2017-12-19 20:33:04 +0000

    Added Message Log

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var messageLog = [];
+
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 })
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  // on connection, emit log
+  io.emit('catch up', messageLog );
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
   socket.on('chat message', function(msg){
+    messageLog.push( msg );
     io.emit('chat message', msg);
   });
 });
