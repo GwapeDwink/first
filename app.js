03e2de372a950b938e3dd04239dcdd3d283f2714
GwapeDwink <GwapeDwink@users.noreply.github.com>
2017-12-19 16:25:21 +0000

    Changed app

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-  var port = process.env.PORT || 3000;
-  app.listen(port, "0.0.0.0", function() {
-  console.log("Listening on Port 3000");
-  });
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
