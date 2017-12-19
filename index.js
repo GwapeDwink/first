c0b92764d5261d831160ba06ec70d88d6a6ae23e
GwapeDwink <GwapeDwink@users.noreply.github.com>
2017-12-19 17:20:47 +0000

    Trying

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
