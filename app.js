07b0243d72b6158b293aa66ff1eebf7937e0b40d
GwapeDwink <GwapeDwink@users.noreply.github.com>
2017-12-19 16:54:01 +0000

    Changed app

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+  
+var app = require('express')();
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -7,6 +9,10 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
+});
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
 });
 
 server.listen(port, hostname, () => {
